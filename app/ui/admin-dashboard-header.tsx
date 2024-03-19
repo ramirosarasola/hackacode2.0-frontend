6c6b74c89cd16c9750db9ad4e6aaf84090f9adf0
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-19 12:27:13 -0300

    Get user and employee by user id

--- app/ui/admin-dashboard-header.tsx
+++ app/ui/admin-dashboard-header.tsx
@@ -2,21 +2,31 @@
 
 import { usePathname } from "next/navigation";
 import { menuItems } from "../(auth)/routes";
-// import { useAppSelector } from "@/lib/hooks";
+import { useAppSelector } from "@/lib/hooks";
 import { BellOutlined, DownOutlined } from "@ant-design/icons";
 import UserProfile from "./user-profile";
+import { fetchEmployeeById } from "@/lib/slices/employeeSlice";
+import { useEffect } from "react";
+import { useAppDispatch } from "@/lib/hooks";
+import { loadUser } from "@/lib/slices/authSlice";
 
 const AdminDashboardHeader = () => {
   const url = usePathname();
-  // const { user } = useAppSelector((state) => state.auth);
-  // test user
+  const dispatch = useAppDispatch();
+  const { user } = useAppSelector((state) => state.auth);
+  const { employee } = useAppSelector((state) => state.employee);
 
-  const user = {
-    name: "John",
-    lastname: "Doe",
-    position: "Developer",
-  };
+  useEffect(() => {
+    dispatch(loadUser()).then((result) => {
+      if(result.payload){
+        dispatch(fetchEmployeeById(user.data.id));
+      }
+    })
+  }, [dispatch]);
 
+  console.log(user)
+  console.log(employee)
+ 
   return (
     <div className="h-[10vh] w-full px-10 flex justify-between items-center ">
       <div className="icon-path text-xl font-bold flex gap-4">
@@ -34,9 +44,9 @@ const AdminDashboardHeader = () => {
       <div className="flex gap-4 items-center ">
         <BellOutlined style={{ fontSize: "24px", color: "#6A6E83" }} />
         <UserProfile
-          name={user.name}
-          lastname={user.lastname}
-          position={user.position}
+          name={employee[0].name}
+          lastname={employee[0].lastname}
+          position={employee[0].position}
           size={"large"} />
         <DownOutlined style={{ fontSize: "16px", color: "#1F1F1F" }} />
       </div>
