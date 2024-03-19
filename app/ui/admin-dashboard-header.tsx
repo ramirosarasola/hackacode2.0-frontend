755c81261660c020c9e4c082c5bf9992c14f50ce
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-19 12:53:59 -0300

    Remove console.logs and logout

--- app/ui/admin-dashboard-header.tsx
+++ app/ui/admin-dashboard-header.tsx
@@ -6,18 +6,23 @@ import { BellOutlined, DownOutlined } from "@ant-design/icons";
 import UserProfile from "./user-profile";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { logout } from "@/lib/slices/authSlice";
+import { useEffect } from "react";
+import { fetchEmployeeById } from "@/lib/slices/employeeSlice";
 
 const AdminDashboardHeader = () => {
   const url = usePathname();
   const { user } = useAppSelector((state) => state.auth);
+  const { employee } = useAppSelector((state) => state.employee);
   const dispatch = useAppDispatch();
   const router = useRouter();
 
-  const handleLogOut = () => {
-    dispatch(logout());
-    router.push("/");
-  };
- 
+  const userId = user?.id;
+  useEffect(() => {
+    if(userId){
+      dispatch(fetchEmployeeById(userId));
+    }
+  }, [userId])
+   
   return (
     <div className="h-[10vh] w-full px-10 flex justify-between items-center ">
       <div className="icon-path text-xl font-bold flex gap-4">
@@ -34,12 +39,16 @@ const AdminDashboardHeader = () => {
       </div>
       <div className="flex gap-4 items-center ">
         <BellOutlined style={{ fontSize: "24px", color: "#6A6E83" }} />
-        <UserProfile
-          name={user?.email}
-          lastname={''}
-          position={''}
-          size={"large"} />
-        <DownOutlined style={{ fontSize: "16px", color: "#1F1F1F", cursor:'pointer' }} onClick={handleLogOut} />
+        {
+          employee && <>
+          <UserProfile
+            name={employee[0].name} 
+            lastname={employee[0].lastname}
+            position={employee[0].position}
+            size={"large"} />
+          <DownOutlined style={{ fontSize: "16px", color: "#1F1F1F", cursor:'pointer' }} />
+          </>
+        }
       </div>
     </div>
   );
