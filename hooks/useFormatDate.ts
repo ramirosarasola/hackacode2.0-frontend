7c9c89969b67e86b084bfb7cd92d0d6dc7546d15
ramirosarasola <ramiro.sarasola@gmail.com>
2024-03-22 19:07:41 -0300

    fix useFormatDate hook

--- hooks/useFormatDate.ts
+++ hooks/useFormatDate.ts
@@ -6,7 +6,7 @@ const useFormatDate = (initialDate: string): string => {
   useEffect(() => {
     const formatDate = (dateString: string): string => {
       const date = new Date(dateString);
-      const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+      const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
       return date.toLocaleDateString('es-ES', options);
     };
 
