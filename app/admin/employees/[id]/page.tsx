d675ddbb244a4a66d5b60c178af931921ad5eb57
mateoghidini1998 <mateghidini123@gmail.com>
2024-03-25 00:52:39 -0300

    Remove uneccessary data

--- app/admin/employees/[id]/page.tsx
+++ app/admin/employees/[id]/page.tsx
@@ -16,8 +16,7 @@ export default function EmployeePage() {
   const { users } = useAppSelector((state) => state.auth);
   const { employee }: any = useAppSelector((state) => state.employee);
   const { saleByEmployee }: any = useAppSelector((state) => state.sale);
-  const userEmployee =
-    users.find((user) => user.id === employee?.user_id) || null;
+  
 
   useEffect(() => {
     dispatch(fetchEmployee(id));
@@ -25,9 +24,12 @@ export default function EmployeePage() {
     dispatch(getSalesByEmployee(id));
   }, [dispatch]);
 
+  const userEmployee =
+    users?.find((user) => user.id === employee?.user_id) || null;
+
   if (!employee) {
     <h1>Loading...</h1>;
-  }
+  } 
 
   return (
     <section className="bg-white p-6 flex flex-col justify-between items-center gap-16 text-[#000]">
@@ -54,8 +56,8 @@ export default function EmployeePage() {
                   : ""}
               </div>
               <h5>{`${employee?.name} ${employee?.lastname}`}</h5>
-              <Tag className="border-[#5A81FA] border-[1.5px] rounded-full">
-                # {employee?.id}
+              <Tag className="border-[#5A81FA] text-[#5A81FA] bg-transparent border-[1.5px] m-0 py-[3px] px-[10px] rounded-full">
+                {employee?.position}
               </Tag>
             </div>
 
@@ -109,98 +111,21 @@ export default function EmployeePage() {
           </div>
           <ul className="profile-section w-[90%] m-auto h-full flex flex-col items-start  justify-center gap-4">
             <li className="w-full flex gap-2">
-              <span className="w-[140px] text-[#CDDEFF]">Amount of Sales:</span>{" "}
+              <span className="w-[140px] text-[#A8B1CF]">Amount of Sales:</span>{" "}
               {saleByEmployee && saleByEmployee.count}
             </li>
             <li className="w-full flex gap-2">
-              <span className="w-[140px] text-[#CDDEFF]">Total Profit:</span>
+              <span className="w-[140px] text-[#A8B1CF]">Total Profit:</span>
               {"$ "}
               {saleByEmployee && saleByEmployee.total}
             </li>
             <li className="w-full flex gap-2">
-              <span className="w-[140px] text-[#CDDEFF]">Account Number:</span>{" "}
+              <span className="w-[140px] text-[#A8B1CF]">Account Number:</span>{" "}
               {employee?.dni}
             </li>
             <li className="w-full flex gap-2">
-              <span className="w-[140px] text-[#CDDEFF]">SSN:</span>{" "}
+              <span className="w-[140px] text-[#A8B1CF]">SSN:</span>{" "}
               {employee?.dni}
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      {/* ProfileContent */}
-      <div className=" w-full flex gap-8">
-        {/* Job Details */}
-        <article className="w-[70%] h-[315px]  border border-[#CDDEFF] rounded-lg ">
-          <div className="card-title h-[75px] border-b border-[#cddeff] text-[#000] flex px-6 items-center">
-            Job Details
-          </div>
-
-          <div className="profile-section px-6 flex justify-between items-center">
-            <div className="flex gap-4 h-full items-center mx-auto justify-between py-4">
-              <div className="flex flex-col gap-2">
-                {/* Information */}
-                <p>
-                  <span className="text-[#A8B1CF] "> First Name: </span>{" "}
-                  {employee?.name}
-                </p>
-                <p>
-                  <span className="text-[#A8B1CF] "> Last Name: </span>{" "}
-                  {employee?.lastname}
-                </p>
-                <p>
-                  <span className="text-[#A8B1CF] "> Position: </span>{" "}
-                  {employee?.position}
-                </p>
-                <p>
-                  <span className="text-[#A8B1CF] "> Salary: </span> $
-                  {employee?.salary}
-                </p>
-              </div>
-              <div className="flex flex-col gap-2">
-                {/* Information */}
-                <p>
-                  <span className="text-[#A8B1CF] "> Date of Birth: </span>{" "}
-                  {employee?.birthdate}
-                </p>
-                <p>
-                  <span className="text-[#A8B1CF] "> Nacionality: </span>{" "}
-                  {employee?.country}
-                </p>
-                <p>
-                  <span className="text-[#A8B1CF] "> Email Adress: </span>{" "}
-                  {userEmployee?.email}
-                </p>
-                <p>
-                  <span className="text-[#A8B1CF] "> Phone: </span>{" "}
-                  {employee?.phone}
-                </p>
-              </div>
-            </div>
-          </div>
-        </article>
-        {/* Address */}
-        <div className="w-[30%] h-[315px]  border border-[#CDDEFF] rounded-lg ">
-          <div className="card-title h-[75px] border-b border-[#cddeff] text-[#000] flex px-6 items-center">
-            Address
-          </div>
-          <ul className="profile-section w-[90%] m-auto h-full flex flex-col items-start  justify-center gap-4">
-            <li className="w-full flex gap-2">
-              <span className="w-[140px] text-[#CDDEFF]">Address:</span>{" "}
-              {employee?.address}
-            </li>
-            <li className="w-full flex gap-2">
-              <span className="w-[140px] text-[#CDDEFF]">Address:</span>{" "}
-              {employee?.address}
-            </li>
-            <li className="w-full flex gap-2">
-              <span className="w-[140px] text-[#CDDEFF]">Address:</span>{" "}
-              {employee?.address}
-            </li>
-            <li className="w-full flex gap-2">
-              <span className="w-[140px] text-[#CDDEFF]">Address:</span>{" "}
-              {employee?.address}
             </li>
           </ul>
         </div>
